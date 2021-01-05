b486aeadab0416b7607c9b98866c1ae3a27166db
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-01-05 20:07:32 -0300

    refactor

--- index.js
+++ index.js
@@ -19,24 +19,21 @@ client.on('message',async message => {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    if(command == 'ping'){
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-        return;
-    }
-    if (command === "sum") {
-        const numArgs = args.map(x => parseFloat(x));
-        const sum = numArgs.reduce((counter, x) => counter += x);
-        message.reply(`The sum of all the arguments you provided is ${sum}!`);
-        return;
-    }
-    if(command == 'grongos'){
-        await play.firework(message);
-    }
-    if(command == 'grongos'){
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-        return;
+    switch (command){
+        case 'grongos':
+            await play.firework(message);
+            return;
+        case 'sum':
+            const numArgs = args.map(x => parseFloat(x));
+            const sum = numArgs.reduce((counter, x) => counter += x);
+            message.reply(`The sum of all the arguments you provided is ${sum}!`);
+            return;
+        case 'ping':
+            const timeTaken = Date.now() - message.createdTimestamp;
+            message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+            return;
+        default:
+            return;
     }
     
 });
