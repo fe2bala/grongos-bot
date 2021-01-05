ef81908e2347dfbb47b00fde48ad5d80d630eb8f
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-01-05 17:07:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# grongos-bot" 
---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+const prefix = "!";
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('message',async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if(command == 'ping'){
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
+    if (command === "sum") {
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+    }
+    if (command === 'grongos') {
+        await playFirework(message);
+    }
+});
+
+
+
+client.login(config.BOT_TOKEN);
+
+
+
+async function playFirework(message) {
+    if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(ytdl("https://www.youtube.com/watch?v=eXhPThYwwTQ"), {
+            volume: 1,
+        });
+        dispatcher.on('finish', () => {
+            console.log('Finished playing firework!');
+        });
+
+    } else {
+        message.reply('Você precisa estar em um canal de voz primeiro');
+    }
+}
+
