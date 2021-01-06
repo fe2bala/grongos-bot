9d50ebe34ff3a8fa1107078fec62836c474f5efc
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-01-06 00:40:44 -0300

    add tilted

--- commands/play.js
+++ commands/play.js
@@ -15,8 +15,8 @@ module.exports = {
         } else {
             message.reply('Você precisa estar em um canal de voz primeiro');
         }
-    }
-    async firework(message) {
+    },
+    async tilted(message) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
             const dispatcher = connection.play(ytdl("https://www.youtube.com/watch?v=YaJ_a2-zG9M"), {
