7827e2323858f729fcd1fe988cfee53ddc935517
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-01-06 23:14:41 -0300

    teste comando de ajuda

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+const Command = require("../infra/Command");
+
+module.exports = class Help extends Command {
+  constructor () {
+    super()
+
+    this.name = 'help',
+    this.description = 'é o grongos porra!'
+  }
+
+  async run ({ message, client }) {
+    message.channel.send(new MessageEmbed()
+    .setTitle(`Help`)
+    .setDescription(client.commands.reduce(s, command=> s + "!" + command,"Comandos: "))
+  }
+}
