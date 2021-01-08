7aa2d19bc8f9ceaab5faddbc1b3c396f2f5cb5db
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-01-07 21:33:21 -0300

    add monica

---
+++ commands/monica.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+const Command = require("../infra/Command");
+const { execute } = require("../utils/play");
+
+module.exports = class Fart extends Command {
+  constructor () {
+    super()
+
+    this.name = 'monica',
+    this.description = 'é o grongos porra!'
+  }
+
+  async run ({ message, client }) {
+    const videoUrl = "https://www.youtube.com/watch?v=BMNDv2aZid0";
+    await execute(message, videoUrl);
+  }
+}
