f9d45a8d7ca43e7d47b56419b215d1e665c22bf1
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-01-23 01:46:17 -0300

    mudando video

--- commands/monica.js
+++ commands/monica.js
@@ -11,7 +11,7 @@ module.exports = class Monica extends Command {
   }
 
   async run ({ message, client }) {
-    const videoUrl = "https://www.youtube.com/watch?v=82j-FNjdUmA";
+    const videoUrl = "https://www.youtube.com/watch?v=BMNDv2aZid0";
     await execute(message, videoUrl);
   }
 }
